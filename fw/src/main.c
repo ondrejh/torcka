d497eb0a65b603f24061f28d54f1c0fa03a563b4
Ondrej Hejda <ondrejh.ck@email.cz>
2026-01-14 15:16:14 +0100

    random flame like pixels

--- fw/src/main.c
+++ fw/src/main.c
@@ -8,6 +8,30 @@
 
 #define PIN_ONBOARD_LED 25
 
+#include "pico/rand.h"
+
+uint32_t rand_range(uint32_t min, uint32_t max) {
+  return min + (get_rand_32() % (max - min + 1));
+}
+
+void rnd2col(uint32_t rnd, uint8_t *r, uint8_t *g, uint8_t *b) {
+  if (rnd > 511) {
+    *b = rnd - 511;
+    *g = 255;
+    *r = 255;
+  }
+  else if (rnd > 255) {
+    *b = 0;
+    *g = rnd - 255;
+    *r = 255;
+  }
+  else {
+    *b = 0;
+    *g = 0;
+    *r = rnd;
+  }
+}
+
 const float adc2vbat = 4.2f / 2334; // 4.2V ~ 2334 raw adc 
 const float adc2vusb = 3.3f / (1 << 12) * 3.0f;
 
@@ -43,8 +67,13 @@ int main() {
       tLed = now;
       cnt += 1;
       cnt %= NUM_LEDS;
-      for (int i=0; i < NUM_LEDS; i++)
-        put_pixel(urgb_u32((cnt == i) ? 10 : 0, 0, 0));
+      for (int i=0; i < NUM_LEDS; i++) {
+        uint32_t rnd = rand_range(0, 3*256 - 1);
+        uint8_t r, g, b;
+        rnd2col(rnd, &r, &g, &b);
+        put_pixel(urgb_u32(r, g, b));
+        //put_pixel(urgb_u32((cnt == i) ? 10 : 0, 0, 0));
+      }
     }
 
     if ((now - tAdc) > 1000) {
